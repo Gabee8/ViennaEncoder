513934486a201e11243b7716007f853eeffd8b9a
Gabee8 <gabee8@citromail.hu>
2017-09-22 15:04:26 +0200

    Add files via upload

---
+++ ViennaEncoder/AboutControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+
+
+namespace ViennaEncoder
+{
+    public partial class AboutControl : UserControl
+    {
+        public AboutControl()
+        {
+            InitializeComponent();
+            string company = ((AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyCopyrightAttribute), false)).Copyright;
+            versionlabel.Text = Assembly.GetExecutingAssembly().GetName().Version.ToString() + " " + company;
+        }
+    }
+}
